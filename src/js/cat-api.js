b1d7e06e163c097acf92015e3045ab611f0595c4
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-11-03 00:39:03 +0200

    Feat(Files) Finish homework

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -6,7 +6,9 @@ axios.defaults.headers.common['x-api-key'] =
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+const errorInfo = document.querySelector('.error');
+
+errorInfo.style.color = 'red';
 
 function hideElement(element) {
   element.style.display = 'none';
@@ -16,71 +18,77 @@ function showElement(element) {
   element.style.display = 'block';
 }
 
-async function fetchBreeds() {
+function fetchBreeds() {
   showElement(loader);
-  hideElement(catInfo);
-  hideElement(error);
+  hideElement(breedSelect);
+  hideElement(errorInfo);
 
-  try {
-    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
-    return response.data;
-  } catch (error) {
-    throw error;
-  } finally {
-    hideElement(loader);
-  }
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      showElement(breedSelect);
+      return response.data;
+    })
+    .catch(error => {
+      showElement(errorInfo);
+      throw error;
+    })
+    .finally(() => hideElement(loader));
 }
 
-async function fetchCatByBreed(breedId) {
+function fetchCatByBreed(breedId) {
   showElement(loader);
-  hideElement(catInfo);
-  hideElement(error);
+  hideElement(errorInfo);
 
-  try {
-    const response = await axios.get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
-  } finally {
-    hideElement(loader);
-  }
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(error => {
+      showElement(errorInfo);
+      throw error;
+    })
+    .finally(() => hideElement(loader));
 }
 
 function displayCatInfo(catData) {
   catInfo.innerHTML = `
-    <img src="${catData[0].url}" alt="Cat Image" style="width: 350px; height: 350px;" />
+    <div class="wrapper">
+    <img class="cat-img" src="${catData[0].url}" alt="Cat Image"/>
+    <div>
     <h2>${catData[0].breeds[0].name}</h2>
-    <p>Description: ${catData[0].breeds[0].description}</p>
-    <p>Temperament: ${catData[0].breeds[0].temperament}</p>
+    <p><b>Description:</b> ${catData[0].breeds[0].description}</p>
+    <p><b>Temperament:</b> ${catData[0].breeds[0].temperament}</p>
+    </div>
+    </div>
   `;
   showElement(catInfo);
 }
 
-async function handleBreedSelection() {
+function handleBreedSelection() {
   const selectedBreedId = breedSelect.value;
 
-  try {
-    const catData = await fetchCatByBreed(selectedBreedId);
-    displayCatInfo(catData);
-  } catch (error) {
-    showElement(error);
-  }
+  fetchCatByBreed(selectedBreedId)
+    .then(catData => displayCatInfo(catData))
+    .catch(error => {
+      showElement(errorInfo);
+      throw error;
+    });
 }
 
-export async function initializeApp() {
-  try {
-    const breeds = await fetchBreeds();
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      breedSelect.appendChild(option);
+export function initializeApp() {
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+    })
+    .catch(error => {
+      showElement(errorInfo);
+      throw error;
     });
-  } catch (error) {
-    showElement(error);
-  }
 
   breedSelect.addEventListener('change', handleBreedSelection);
 }
